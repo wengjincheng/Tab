bbe05b684a58cef371e89d41c506f2f9a8de474f
天真娃 <wengjin456123@gmail.com>
2016-11-01 16:09:59 +0800

    Add files via upload

---
+++ Exercise/js/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by 翁锦程 on 2016/11/1.
+ */
+$('#ul_1').on('click',function (e) {
+    var x = $(e.target).html();
+    if(x=='Tab One'){
+        $('.li_1').addClass('background_color');
+        $('.li_2').removeClass('background_color');
+        $('.li_3').removeClass('background_color');
+        $('.li_4').removeClass('background_color');
+        $('#p_1').html('This is the one');
+    }else if(x=='Tab Two'){
+        $('.li_2').addClass('background_color');
+        $('.li_1').removeClass('background_color');
+        $('.li_3').removeClass('background_color');
+        $('.li_4').removeClass('background_color');
+        $('#p_1').html('This is the two');
+    }else if(x=='Tab Three'){
+        $('.li_3').addClass('background_color');
+        $('.li_1').removeClass('background_color');
+        $('.li_2').removeClass('background_color');
+        $('.li_4').removeClass('background_color');
+        $('#p_1').html('This is the three');
+    }else if(x=='Tab Four'){
+        $('.li_4').addClass('background_color');
+        $('.li_1').removeClass('background_color');
+        $('.li_2').removeClass('background_color');
+        $('.li_3').removeClass('background_color');
+        $('#p_1').html('This is the four');
+    }
+})
